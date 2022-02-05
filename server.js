c9b60f1535f71729972bf2c6baa61a6ce085ab9e
Bianca Pimenta <bi.cp1802@gmail.com>
2022-02-05 11:53:11 +0100

    commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+
+mongoose.connect("mongodb://localhost/blog-v2");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const userRouter = require("./routes/user.routes");
+app.use("/user", userRouter);
+
+app.listen(3000);
